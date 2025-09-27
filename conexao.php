de04206e2576aba16b47035b77b908457eebf8c2
andreerfr <andreerci@gmail.com>
2025-09-27 09:20:17 -0300

    conexão

--- conexao.php
+++ conexao.php
@@ -2,12 +2,12 @@
 // Conexão com o banco (contém erro de variável e de conexão)
 $host = "localhost";
 $user = "root";
-$password = "";
+$password = "root"; //vazio, não precisava de nada, mas é bom ter
 $db = "crud_exemplo";
 
 $conn = mysqli_connect($hot, $user, $password, $db); // Erro: $hot ao invés de $host
 
-if (!$conn) {
-    echo "Falha na conexão!";
+if ($conn->connect_error) { //alterção na logica
+    die("Falha de conexão: " .$sconn->connect_error); //adição para mensagem de erro
 }
 ?>
